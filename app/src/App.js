9d8e85e8e31524fb2f5ce117bf4df45952958396
Sara Alshreef <S.a.r.a.h768700@gmail.com>
2023-03-19 16:25:37 +0300

    need styling + new img from Mojahid

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,8 @@
 import './App.css';
 import img from './images/img.jpg'
 import React from 'react';
-
+import { toPng } from 'html-to-image'
+import download from 'downloadjs'
 function App() {
   const [text, setText] = React.useState("")
 
@@ -10,14 +11,22 @@ function App() {
     const newText = event.target.value
     setText(newText)
   }
+const node = document.getElementById('image-download')
+function downloadImage () {
+toPng(node).then (dataURL => {
+  download(dataURL, "congrats.png")
+})
+.catch(() => console.log("Erorr here #19"))
+}
 
   return (
     <div className="container">
       <input type="text" placeholder="Type text here" className="input" onChange={handleText} value={text} />
-      <div className="image-wrapper">
+      <div className="image-wrapper" id="image-download">
         <img src={img} alt="" className="image" />
         <h1 className="image-text">{text}</h1>
       </div>
+      <button className="download-btn" onClick={downloadImage}>download</button>
     </div>
   );
 }
